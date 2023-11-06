adaf25973ce1ba5370b95f154b3339eaa1bbb3b9
DivyaBadgujar25 <145791935+DivyaBadgujar25@users.noreply.github.com>
2023-11-06 16:42:46 +0530

    Create Q1.c

---
+++ Q1.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+
+
+int main() {
+
+    int a1[10];
+
+    
+
+    printf("Enter 10 elements:\n");
+
+    for (int i = 0; i < 10; i++) {
+
+        scanf("%d", &a1[i]);
+
+    }
+
+
+
+    printf("Elements in reverse order:\n");
+
+    for (int i = 9; i >= 0; i--) {
+
+        printf("%d\n", a1[i]);
+
+    }
+
+
+
+    return 0;
+
+}

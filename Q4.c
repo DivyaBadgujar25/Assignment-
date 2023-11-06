974e0d8413fe1ad829fd10d93e03c099ffd7382d
DivyaBadgujar25 <145791935+DivyaBadgujar25@users.noreply.github.com>
2023-11-06 16:46:16 +0530

    Create Q4.c

---
+++ Q4.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+
+
+struct State {
+
+    char name[100];
+
+    int population;
+
+    float lit_rate;
+
+    float income;
+
+};
+
+
+
+int main() {
+
+    struct State st1;
+
+
+
+    strcpy(st1.name, "Maharashtra");
+
+    st1.population = 1000000;
+
+    st1.lit_rate = 5.8;
+
+    st1.income = 6000.00;
+
+
+
+    printf("State Name: %s\n", st1.name);
+
+    printf("Population: %d\n", st1.population);
+
+    printf("Literacy Rate: %.2f\n", st1.lit_rate);
+
+    printf("Income: %.2f\n", st1.income);
+
+
+
+    return 0;
+
